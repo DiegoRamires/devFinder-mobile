233efeb1b871ed9e231d098d961ae936d7d60e7b
DiegoRamires <goncalves.ramires@gmail.com>
2020-01-16 20:30:11 -0300

    redireting user to profile page when click on callout

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,7 +6,7 @@ import {
   getCurrentPositionAsync
 } from "expo-location";
 
-function Main() {
+function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
@@ -46,7 +46,11 @@ function Main() {
           }}
         />
 
-        <Callout>
+        <Callout
+          onPress={() => {
+            navigation.navigate("Profile", { github_username: "diegoramires" });
+          }}
+        >
           <View style={styles.callout}>
             <Text style={styles.devName}>Diego Ramires</Text>
             <Text style={styles.devBio}>Desenvolvedor FrontEnd</Text>
