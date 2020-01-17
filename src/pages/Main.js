f46e48e8824b732fd8dcd3f7ef2e01f32a05739c
DiegoRamires <goncalves.ramires@gmail.com>
2020-01-17 12:06:59 -0300

    create and style search form and button

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 import MapView, { Marker, Callout } from "react-native-maps";
 import {
   requestPermissionsAsync,
@@ -37,28 +45,47 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -31.8420959, longitude: -52.8202654 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://avatars3.githubusercontent.com/u/7052300?s=460&v=4"
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -31.8420959, longitude: -52.8202654 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://avatars3.githubusercontent.com/u/7052300?s=460&v=4"
+            }}
+          />
+
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "diegoramires"
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Diego Ramires</Text>
+              <Text style={styles.devBio}>Desenvolvedor FrontEnd</Text>
+              <Text style={styles.devTechs}>
+                ReactJS, React Native, Node.js
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Burcar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
 
-        <Callout
-          onPress={() => {
-            navigation.navigate("Profile", { github_username: "diegoramires" });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Diego Ramires</Text>
-            <Text style={styles.devBio}>Desenvolvedor FrontEnd</Text>
-            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -91,6 +118,41 @@ const styles = StyleSheet.create({
 
   devTechs: {
     marginTop: 5
+  },
+
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8e4dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
   }
 });
 
